87f5c7c8ea3a687653a850e6f389e437eeac0feb
niels-van-den-broeck <niels@codecompanion.be>
2023-03-26 20:07:49 +0200

    cleanup

--- core/routes.py
+++ core/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 from core.models import Articles
+from sqlalchemy import select
 
 routes = Blueprint( 'routes', __name__)
 
@@ -9,6 +10,22 @@ def home():
 
 @routes.route('/articles')
 def articles():
-    articles = Articles.query.all()
+    articles = select(Articles).all()
+    session.test = True
 
     return jsonify([article.serialize for article in articles])
+
+# @app.route('/login', methods=['POST'])
+# def loginHandler():
+#     username = request.json.username
+#     password = request.json.password
+
+#     user = query('SELECT * FROM users WHERE username = ? ', username)
+
+#     if user is None:
+#         return 'User not found', 400
+
+#     if hash_matches(password, user.password_hash):
+#         session['user_id'] = user.id # save user id in session so we know who is logged in
+
+#         return 'Logged in', 200
